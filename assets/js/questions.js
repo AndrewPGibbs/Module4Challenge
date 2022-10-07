2aa130c568bef1999ffc876af9986acbffe6f0cd
Andrew PG <andrewpgibbs71@gmail.com>
2022-10-06 22:56:55 -0400

    adding basic framework for project 4

---
+++ assets/js/questions.js
@@ -0,0 +1,49 @@
+
+
+var questions = [
+    {
+        title: 'title',
+        choices: [
+            { text: 'answer1', correct: false},
+            { text: 'answer2', correct: false},
+            { text: 'answer3', correct: true},
+            { text: 'answer4', correct: false}
+        ]
+    },
+    {
+        title: 'title',
+        choices: [
+            { text: 'answer1', correct: false},
+            { text: 'answer2', correct: false},
+            { text: 'answer3', correct: true},
+            { text: 'answer4', correct: false}
+        ]
+    },
+    {
+        title: 'title',
+        choices: [
+            { text: 'answer1', correct: false},
+            { text: 'answer2', correct: false},
+            { text: 'answer3', correct: true},
+            { text: 'answer4', correct: false}
+        ]
+    },
+    {
+        title: 'title',
+        choices: [
+            { text: 'answer1', correct: false},
+            { text: 'answer2', correct: false},
+            { text: 'answer3', correct: true},
+            { text: 'answer4', correct: false}
+        ]
+    },
+    {
+        title: 'title',
+        choices: [
+            { text: 'answer1', correct: false},
+            { text: 'answer2', correct: false},
+            { text: 'answer3', correct: true},
+            { text: 'answer4', correct: false}
+        ]
+    },
+]
