ed0b6ba87e2cdc6b81322ce7774e81ad1821f661
Andrew PG <andrewpgibbs71@gmail.com>
2022-12-07 14:03:16 -0500

    adding onto highscore js file

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,5 +1,7 @@
 function showScore() {
     var score = JSON.parse(localStorage.getItem("highScore"));
     score.sort( function (x, y) { return y.score - x.score;} );
-    
+     for( var i = 0; i<score.length; i++) {
+        var scoreList = document.createElement('li');
+     }
 }
