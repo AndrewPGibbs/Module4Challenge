3923a2e3d8069ca628b1e0da354a5ec6ac015dbe
Andrew PG <andrewpgibbs71@gmail.com>
2023-03-13 22:18:54 -0400

    pagechange function added to swap between pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-let startBtn = document.querySelector("start-button");
+let startBtn = document.getElementById("start-button");
+let nextBtn = document.getElementById("next-button");
+let firstPage = document.getElementById("page-1");
+let secondPage = document.getElementById("page-2");
+let thirdPage = document.getElementById("page-3");
+let allPages = [firstPage, secondPage, thirdPage];
+let i = 0;
+
+function pageChange() {
+ 
+  allPages[i].classList.add("hide");
+  i++;
+  allPages[i].classList.remove("hide");
+  console.log(i);
+}
+
+function startQuiz() {
+  pageChange();
+  console.log(i);
+}
+
+startBtn.addEventListener("click", startQuiz);
+nextBtn.addEventListener("click", pageChange);
